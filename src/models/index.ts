df7397b77be7e09ea4e12a3e6490bd00185a3172
naman9271 <namanjain9271@gmail.com>
2025-08-21 17:16:58 +0530

    fixup

--- src/models/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all models for easy importing
-export { default as User } from './User';
-export { default as Game } from './Game';
-export { default as Score } from './Score';
-
-// Export interfaces for TypeScript
-export type { IUser } from './User';
-export type { IGame } from './Game';
-export type { IScore } from './Score';
